6a73c80584c1507c4a81ec5b73b934289cdbc5b2
Mark Yap <markyap_1@yahoo.com>
2017-07-12 00:55:27 +0800

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,6 +1,6 @@
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === <comawhite>) {
+      req.query['hub.verify_token'] === 'comawhite') {
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
